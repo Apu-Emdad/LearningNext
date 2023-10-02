8dba82f2c7885e8ebc550cf326f8719766977047
Emdadul Haque Apu <emdadul.apu@savethechildren.org>
2023-10-02 15:52:10 +0600

    destructuring context

--- learning-next-part-two/src/pages/news/[category].js
+++ learning-next-part-two/src/pages/news/[category].js
@@ -20,11 +20,11 @@ const ArticlesByCategory = ({ articles, category }) => {
 export default ArticlesByCategory;
 
 export const getServerSideProps = async (context) => {
-  const { params } = context;
-  console.log('params:', params);
+  const { params, req, res, query } = context;
+
   const { category } = params;
-  const res = await fetch(`http://localhost:4000/news?category=${category}`);
-  const data = await res.json();
+  const response = await fetch(`http://localhost:4000/news?category=${category}`);
+  const data = await response.json();
 
   return {
     props: {
